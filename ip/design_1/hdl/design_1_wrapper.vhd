72327b9d140db364ccfccc968bd7990946a2f519
gaawa <17564966+gaawa@users.noreply.github.com>
2024-05-22 13:34:01 +0200

    initial commit

---
+++ ip/design_1/hdl/design_1_wrapper.vhd
@@ -0,0 +1,23 @@
+--Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2021.1 (win64) Build 3247384 Thu Jun 10 19:36:33 MDT 2021
+--Date        : Wed May 22 12:43:43 2024
+--Host        : LAPTOP-N26RM7D0 running 64-bit major release  (build 9200)
+--Command     : generate_target design_1_wrapper.bd
+--Design      : design_1_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_wrapper is
+end design_1_wrapper;
+
+architecture STRUCTURE of design_1_wrapper is
+  component design_1 is
+  end component design_1;
+begin
+design_1_i: component design_1
+ ;
+end STRUCTURE;
